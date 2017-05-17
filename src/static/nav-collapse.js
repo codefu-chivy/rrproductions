113a4e5c29bbf179c31aab0cd799b136cf6a1f9d
Chival Trotman <chivy360@gmail.com>
2017-05-17 04:12:00 -0400

    added logic for nav collapse

---
+++ src/static/nav-collapse.js
@@ -0,0 +1,25 @@
+$(function() {
+    let minimize = false;
+    function collapse() {
+        if ($(window).width() <= 430) {
+            $("#col-button").removeClass("collapse");
+        }
+        else {
+            $("#col-button").addClass("collapse");
+        }
+    };
+    collapse();
+    $(window).resize(function() {
+        collapse();
+    }) 
+    $("#col-button").click(function() {
+        minimize = !minimize;
+        if (minimize) {
+            $("#col-button").text("Minimize");
+        }
+        else {
+            $("#col-button").text("Menu");
+        }
+        $(".menu").toggleClass("nav-link-expand");
+    })
+});
