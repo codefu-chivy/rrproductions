2cfa88ebe182500b6add19b18e8a0c1dfad6f952
Chival Trotman <chivy360@gmail.com>
2017-05-18 00:13:36 -0400

    modified about description

--- src/components/about.js
+++ src/components/about.js
@@ -23,7 +23,7 @@ export default class About extends React.Component {
                   <img className="not-found" src="/static/images/noimagefound.jpg"/>
                   <p className="bio">My name is Siahh and I'm a singer, rapper, songwriter from Brooklyn, New York. My style of music stems from the 90s era: very simplistic, to the point and filled with everlasting messages for my listeners. One listen at a time, I'm hoping to change the opinions of many, especially those of the youth.</p>
                 </div>
-                <h2>Chival</h2>
+                <h2>Nick</h2>
                 <div className="about1">
                   <img className="not-found" src="/static/images/noimagefound.jpg"/>
                   <p className="bio">Hey everyone, I'm a composer and musician from West Babylon, New York, and I have a passion for creating great music. The music I create reflects a fusion of several genres, most importantly jazz, electronic, and rock. I believe that good music serves a higher purpose that not only includes engaging the mind but healing the soul and it's my hope that my creations can do that for you.</p>
