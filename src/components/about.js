37548def345fca7c14ae832a29ccb0de403ff106
Chival Trotman <chivy360@gmail.com>
2017-07-04 21:45:05 -0400

    modified artist description

--- src/components/about.js
+++ src/components/about.js
@@ -25,7 +25,7 @@ export default class About extends React.Component {
                 <div className="about1">
                   <p className="bio">My name is Siahh and I'm a singer, rapper, songwriter from Brooklyn, New York. My style of music stems from the 90s era: very simplistic, to the point and filled with everlasting messages for my listeners. One listen at a time, I'm hoping to change the opinions of many, especially those of the youth.</p>
                 </div>
-                <h2>Chival Trotman / Fusion Force</h2>
+                <h2>Fusion Force</h2>
                 <div className="about1">
                   <p className="bio">Hey everyone, I'm a composer and musician also known as Fusion Force from New York, and I have a passion for creating great music. The music I create reflects a fusion of several genres, most importantly jazz, electronic, and rock. I believe that good music serves a higher purpose that not only includes engaging the mind but healing the soul and it's my hope that my creations can do that for you.</p>
                 </div>  
